0bae0d46f42a92a3e1e7290ce0cdc661cb6c83cf
brad <brad>
2008-04-27 20:17:09 +0000

    Finished basic workings to equal version 2

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -11,6 +11,12 @@ class IndexController < ApplicationController
     @sections = Product.find_all_by_category_id(params[:id], :select => "DISTINCT sub_cat")
   end
   
+  def view_product
+    @categories = Category.find(:all)
+    @product = Product.find_by_id(params[:id])
+    @image = Photo.find_by_product_id(params[:id])
+  end
+  
 / These controllers are used to display and render images properly./  
   def thumb
     @photo = Photo.find_by_id(params[:id])
@@ -18,5 +24,9 @@ class IndexController < ApplicationController
   
   def cat
     @photo = Photo.find_by_id(params[:id])
-  end  
+  end 
+  
+  def view
+    @photo = Photo.find_by_id(params[:id])
+  end 
 end
